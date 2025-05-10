c34d87d5ebdca25ca401a9dd35b60e6a60d5a823
mayurm16 <45730637+mayurm16@users.noreply.github.com>
2025-05-10 10:33:27 +0000

    optimized react code

--- src/create/Create.jsx
+++ src/create/Create.jsx
@@ -1,10 +1,19 @@
 import React from "react";
 
+
+
+const inputCollection = [
+  {id:"bookName", name:"bookName", text:"Book Name", placeHolder:"Book name.." },
+  {id:"ISBN", name:"ISBN", text:"ISBN", placeHolder:"ISBN" },
+  {id:"Author", name:"Author", text:"Author", placeHolder:"Author" },
+]
+
 export const Create = () => {
   return (
     <div className="container">
-      <form action="/">
-        <div className="row">
+      <form action="/"> 
+        {/* since all of these  */}
+        {/* <div className="row">
           <div className="col-25">
             <label htmlFor="bookName">Book Name</label>
           </div>
@@ -43,7 +52,30 @@ export const Create = () => {
               placeholder="Author"
             />
           </div>
-        </div>
+        </div> */}
+
+        {/* 
+         since majority of inputs are same this way is better to minize jsx 
+         plus in future if any new inputs are to be added like country then this you don't have to write different jsx again all you'd need is to update the array to include an other collection
+        */}
+        {
+          inputCollection.map(input => (
+          <>
+            <div className="row">
+              <div className="col-25">
+                <label htmlFor={input.id}>{input.text}</label>
+              </div>
+              <div className="col-75">
+                <input
+                type="text"
+                id={input.id}
+                name={input.name}
+                placeholder={input.placeHolder}
+              />
+              </div>
+            </div>
+          </>))
+        }
 
         <div className="row">
           <div className="col-25">
--- src/fetch/Fetch.jsx
+++ src/fetch/Fetch.jsx
@@ -1,64 +1,126 @@
 import React from "react";
 
-const canRender = true;
-function renderData() {
-  if (canRender) {
-    return (
-      <>
-        <tr>
-          <td>Alfreds Futterkiste</td>
-          <td>Maria Anders</td>
-          <td>Germany</td>
-        </tr>
-        <tr>
-          <td>Berglunds snabbköp</td>
-          <td>Christina Berglund</td>
-          <td>Sweden</td>
-        </tr>
-        <tr>
-          <td>Centro comercial Moctezuma</td>
-          <td>Francisco Chang</td>
-          <td>Mexico</td>
-        </tr>
-        <tr>
-          <td>Ernst Handel</td>
-          <td>Roland Mendel</td>
-          <td>Austria</td>
-        </tr>
-        <tr>
-          <td>Island Trading</td>
-          <td>Helen Bennett</td>
-          <td>UK</td>
-        </tr>
-        <tr>
-          <td>Königlich Essen</td>
-          <td>Philip Cramer</td>
-          <td>Germany</td>
-        </tr>
-        <tr>
-          <td>Laughing Bacchus Winecellars</td>
-          <td>Yoshi Tannamuri</td>
-          <td>Canada</td>
-        </tr>
-        <tr>
-          <td>Magazzini Alimentari Riuniti</td>
-          <td>Giovanni Rovelli</td>
-          <td>Italy</td>
-        </tr>
-        <tr>
-          <td>North/South</td>
-          <td>Simon Crowther</td>
-          <td>UK</td>
-        </tr>
-        <tr>
-          <td>Paris spécialités</td>
-          <td>Marie Bertrand</td>
-          <td>France</td>
-        </tr>
-      </>
-    );
-  }
-}
+
+  const companyCollection = [ {
+    isbn:"Alfreds Futterkiste",
+    contact:"Maria Anders",
+    country:"Germany"
+  },
+  {
+    isbn:"Berglunds snabbköp",
+    contact:"Christina Berglunds",
+    country:"Sweden"
+  },
+  {
+    isbn:"Centro comercial Moctezum",
+    contact:"Francisco Chang",
+    country:"Mexico"
+  },
+  {
+    isbn:"Ernst Handel",
+    contact:"Roland Mendel",
+    country:"Austria"
+  },
+      {
+    isbn:"Island Trading",
+    contact:"Helen Bennett",
+    country:"UK"
+  },
+  {
+    isbn:"Königlich Essen",
+    contact:"Philip Cramer",
+    country:"Germany"
+  },
+  {
+    isbn:"Laughing Bacchus Winecellars",
+    contact:"Yoshi Tannamuri",
+    country:"Canada"
+  },
+    {
+    isbn:"Laughing Bacchus Winecellars",
+    contact:"Yoshi Tannamuri",
+    country:"Canada"
+  },
+  {
+    isbn:"Magazzini Alimentari Riuniti",
+    contact:"Giovanni Rovelli", 
+    country:"Italy"
+  },
+    {
+    isbn:"North/South",
+    contact:"Simon Crowther", 
+    country:"UK"
+  },
+    {
+    isbn:"Paris spécialités",
+    contact:"Marie Bertrand", 
+    country:"France"
+  },
+ ];
+ const canRender = true;
+
+// No need to write un-nessary static JSX here
+
+//  function renderData() {  
+//   if (canRender) { 
+//     return (
+//       <>
+//         <tr>
+//           <td>Alfreds Futterkiste</td>
+//           <td>Maria Anders</td>
+//           <td>Germany</td>
+//         </tr>
+//         <tr>
+//           <td>Berglunds snabbköp</td>
+//           <td>Christina Berglund</td>
+//           <td>Sweden</td>
+//         </tr>
+//         <tr>
+//           <td>Centro comercial Moctezuma</td>
+//           <td>Francisco Chang</td>
+//           <td>Mexico</td>
+//         </tr>
+//         <tr>
+//           <td>Ernst Handel</td>
+//           <td>Roland Mendel</td>
+//           <td>Austria</td>
+//         </tr>
+//         <tr>
+//           <td>Island Trading</td>
+//           <td>Helen Bennett</td>
+//           <td>UK</td>
+//         </tr>
+//         <tr>
+//           <td>Königlich Essen</td>
+//           <td>Philip Cramer</td>
+//           <td>Germany</td>
+//         </tr>
+//         <tr>
+//           <td>Laughing Bacchus Winecellars</td>
+//           <td>Yoshi Tannamuri</td>
+//           <td>Canada</td>
+//         </tr>
+//         <tr>
+//           <td>Magazzini Alimentari Riuniti</td>
+//           <td>Giovanni Rovelli</td>
+//           <td>Italy</td>
+//         </tr>
+//         <tr>
+//           <td>North/South</td>
+//           <td>Simon Crowther</td>
+//           <td>UK</td>
+//         </tr>
+//         <tr>
+//           <td>Paris spécialités</td>
+//           <td>Marie Bertrand</td>
+//           <td>France</td>
+//         </tr>
+//       </>
+//     );
+//   }
+// }
+
+
 export const Fetch = () => {
   return (
     <table id="record">
@@ -67,7 +129,18 @@ export const Fetch = () => {
       <th>Published</th>
       <th>Author</th>
     </tr>
-    {renderData()}
+    { canRender && ((  // will execute this code only when canRender variable is true
+      <>
+        {companyCollection.map( companyObj => (
+          <tr key={companyObj?.isbn?.replaceAll(" ", "-") /* this key is to avoid un-nessary re-render of any the same items again and again */ }>
+            <td>{companyObj.isbn}</td>
+            <td>{companyObj.contact}</td>
+            <td>{companyObj.country}</td>
+          </tr>) 
+      )}
+      </>
+    )) 
+    }
   </table>
   );
 };
